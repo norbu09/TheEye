ab1caf250b252e93f3ba348f5073f78616e818da
Lenz Gschwendtner <lenz@springtimesoft.com>
2012-12-15 22:56:59 +1300

    Checking in changes prior to tagging of version 0.7.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index d10bb08..6dd77cb 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for TheEye
    
    +0.7     2012-12-15T22:55:13
    +        first draft of new Graphite helper lib
    +
     0.6     2012-07-24T20:43:03
             made some changes to handle unplanned debug output from
             phantomjs tests

--- lib/TheEye.pm
+++ lib/TheEye.pm
@@ -15,11 +15,11 @@ TheEye - a TAP based monitoring system!
 
 =head1 VERSION
 
-Version 0.6.6.6.5.5.5.5.5.5.5.5.5.5.4
+Version 0.7.7.7.6.6.6.5.5.5.5.5.5.5.5.5.5.4
 
 =cut
 
-our $VERSION = '0.6';
+our $VERSION = '0.7';
 
 has 'test_dir' => (
     is       => 'rw',
