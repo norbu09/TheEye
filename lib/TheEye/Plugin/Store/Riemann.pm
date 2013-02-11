dba45df350838d4c5d9695dda4db099ae604009f
Lenz Gschwendtner <lenz@springtimesoft.com>
2013-02-12 00:43:45 +1300

    added Riemann plugin

---
+++ lib/TheEye/Plugin/Store/Riemann.pm
@@ -0,0 +1,45 @@
+#!/usr/bin/perl -Ilib
+
+package TheEye::Plugin::Store::Riemann;
+
+use Mouse::Role;
+use Riemann::Client;
+
+has 'riemann' => (
+    is       => 'rw',
+    isa      => 'Riemann::Client',
+    required => 1,
+    lazy     => 1,
+    default  => sub { Riemann::Client->new( host => 'localhost', port => 5555)},
+);
+
+
+around 'save' => sub {
+    my $orig = shift;
+    my ( $self, $tests ) = @_;
+
+    my @events;
+    foreach my $result (@{$tests}) {
+
+        my @path = split(/\//, $result->{file});
+        my @file = split(/\./, pop(@path));
+
+        my $event = {
+            state => 'ok',
+            host   => $self->hostname,
+            service => $file[0],
+            time   => $result->{time},
+            metric   => $result->{delta},
+            description => $result->{passed} .' passed and '. $result->{failed} .' failed',
+        };
+        if($result->{failed}){
+            $event->{state} = 'critical';
+        }
+        push(@events, $event);
+    }
+    $self->riemann->send(@events);
+    return;
+};
+
+
+1;
